a49d01adc93a94599ea679ef73d6a2d643a360c5
sudhanshusingh-g <sudsingh32@gmail.com>
2025-02-02 13:13:23 +0530

    views added with templates

--- fullstackapp/tweet/urls.py
+++ fullstackapp/tweet/urls.py
@@ -4,5 +4,8 @@ from django.urls import path
 # from django.conf.urls.static import static
 from . import views
 urlpatterns = [
-    path('', views.index,name=""),
+    path('', views.tweet_list,name="tweet_list"),
+    path('create/', views.create_tweet,name="create_tweet"),
+    path('<int:tweet_id>/edit/', views.edit_tweet,name="edit_tweet"),
+    path('<int:tweet_id>/delete/', views.delete_tweet,name="delete_tweet"),
 ]
